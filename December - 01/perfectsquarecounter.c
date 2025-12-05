3ed2d39cbdc77f009a54570bcd7c00a00e4ff6fe
akshitha162007 <akshithakumar2007@gmail.com>
2025-12-05 18:02:11 +0530

    december 01

---
+++ December - 01/perfectsquarecounter.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void perfectsquarecounter()
+{
+    int n,count=0;
+    printf("enter the number:");
+    scanf("%d",&n);
+    
+    printf("the perfect square number from 1 to %d is:\n",n);
+    for(int i=1;i*i<=n;i++)
+    {
+        int square=i*i;
+        printf("%d ",square);
+        count++;
+    }
+    printf("\nnumber of perfect squares are:%d",count);
+}
+int main()
+{
+    perfectsquarecounter();
+    return 0;
+}
